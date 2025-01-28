0b32b6ada62a42af68d08dd878be3d78dd8f4a86
Salem Almutairi <salemsnsalmutairi@gmail.com>
2025-01-28 10:59:14 +0300

    bottom nav styled

---
+++ src/components/TabBarBackground.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import BTMNav from '../../assets/svg/BTMNav.svg';
+
+const TabBarBackground = () => {
+  return (
+    <View style={styles.container}>
+      <BTMNav width="full" height="120" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: -15,
+    left: -20,
+    right: -20,
+    top: -50,
+    zIndex: 1000,
+  },
+});
+
+export default TabBarBackground; 
